3d233fd2889238cd76d13c1b4d5ce0631e393176
BogartDME <edwin.pietrowski@gmail.com>
2023-01-10 17:27:36 -0500

    delete path added

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,6 @@ const path = require("path");
 const fs = require("fs");
 const uniqueID = require("uniqid");
 
-//api get router
-router.get("/notes", (req, res) => {
-    const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
-    res.send(noteInfo)
-});
 
 //api post router
 router.post("/notes", (req, res) => {
@@ -15,15 +10,33 @@ router.post("/notes", (req, res) => {
     const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
     res.json(noteInfo)
     console.log(noteID);
-
+});   
+//api get router
+router.get("/notes", (req, res) => {
+    const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
+    res.send(noteInfo)
+    
     const userInput = {
         title: req.body.title,
         text: req.body.text,
         id: noteID
     };
+});
+
     
-    noteInfo.push(userInput);
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteInfo));
+
+//delete
+router.delete('/notes/:id', (req, res) => {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); // reads db.json
+    let noteID = savedNotes.filter(x=>x.id!=req.params.id) // returns route with all notes EXCEPT the ID we are 
+
+    
+    console.log("NOTE ID", noteID)
+    console.log("REQ.PARAMS.ID", req.params.id)
+
+
+noteInfo.push(userInput);
+fs.writeFileSync("./db/db.json", JSON.stringify(noteInfo));
 });
 
 module.exports = router;
