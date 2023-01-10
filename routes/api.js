25933342b0e2178ec2abfe53d8e9bf8af07123f1
BogartDME <edwin.pietrowski@gmail.com>
2023-01-10 17:48:40 -0500

    debugging routes in api.js

--- routes/api.js
+++ routes/api.js
@@ -4,39 +4,51 @@ const fs = require("fs");
 const uniqueID = require("uniqid");
 
 
+// //api post router
+// router.post("/notes", (req, res) => {
+//     let noteID = uniqueID();
+//     const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
+//     res.json(noteInfo)
+//     console.log(noteID);
+// });   
+// //api get router
+// router.get("/notes", (req, res) => {
+//     const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
+//     res.send(noteInfo)
+    
+//     const userInput = {
+//         title: req.body.title,
+//         text: req.body.text,
+//         id: noteID
+//     };
+// });
+
 //api post router
 router.post("/notes", (req, res) => {
     let noteID = uniqueID();
-    const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
-    res.json(noteInfo)
-    console.log(noteID);
-});   
-//api get router
-router.get("/notes", (req, res) => {
-    const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
-    res.send(noteInfo)
-    
-    const userInput = {
+  const userInput = {
         title: req.body.title,
         text: req.body.text,
         id: noteID
     };
-});
-
-    
+    let noteInfo = JSON.parse(fs.readFileSync("./db/db.json")) || []
+noteInfo.push(userInput)
+fs.readFileSync("/db/db.json",JSON.stringify(noteInfo))
+    res.json(noteInfo)
+    console.log(noteID);
+});   
 
 //delete
 router.delete('/notes/:id', (req, res) => {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); // reads db.json
-    let noteID = savedNotes.filter(x=>x.id!=req.params.id) // returns route with all notes EXCEPT the ID we are 
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); // 
+    let noteID = savedNotes.filter(x=>x.id!=req.params.id) 
+fs.writeFileSync("./db/db.json", JSON.stringify(noteID));
+});
 
-    
-    console.log("NOTE ID", noteID)
-    console.log("REQ.PARAMS.ID", req.params.id)
 
 
 noteInfo.push(userInput);
 fs.writeFileSync("./db/db.json", JSON.stringify(noteInfo));
-});
+
 
 module.exports = router;
