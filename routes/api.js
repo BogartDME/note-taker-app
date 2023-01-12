c5b432c84bcbd9a351e39c3590ee08eb1a5ef248
BogartDME <edwin.pietrowski@gmail.com>
2023-01-12 18:13:48 -0500

    adjustments to get routes, notes not deleting or posting to db

--- routes/api.js
+++ routes/api.js
@@ -4,24 +4,29 @@ const fs = require("fs");
 const uniqueID = require("uniqid");
 
 
+//api get router
+router.get("/notes", (req, res) => {
+    const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
+    res.send(noteInfo)
+    
+});
+
 //api post router
 router.post("/notes", (req, res) => {
     let noteID = uniqueID();
     const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
     res.json(noteInfo)
     console.log(noteID);
-});   
-//api get router
-router.get("/notes", (req, res) => {
-    const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
-    res.send(noteInfo)
-    
-    const userInput = {
+
+    const newNote = {
         title: req.body.title,
-        text: req.body.text,
-        id: noteID
+        text: req.body.text, 
+        id: noteID,
     };
-});
+    noteInfo.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteInfo));
+
+});  
 
 // //api post router
 // router.post("/notes", (req, res) => {
