5d6a3c1108f1167409f4670ec8fe29fba0a0396d
BogartDME <edwin.pietrowski@gmail.com>
2023-01-17 21:37:45 -0500

    updates pre help api.js is giving error at line 35 stating that noteInfo.push is not a function

--- routes/api.js
+++ routes/api.js
@@ -4,14 +4,23 @@ const fs = require("fs");
 const uniqueID = require("uniqid");
 
 
-//api get router
+ 
+
+// api get router
 router.get("/notes", (req, res) => {
     const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
     res.send(noteInfo)
-    
+
+    // const userInput = {
+    //     title: req.body.title,
+    //     text: req.body.text,
+    //     id: noteID
+    // };
 });
 
-//api post router
+
+
+// api post router
 router.post("/notes", (req, res) => {
     let noteID = uniqueID();
     const noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
@@ -26,7 +35,7 @@ router.post("/notes", (req, res) => {
     noteInfo.push(newNote);
     fs.writeFileSync("./db/db.json", JSON.stringify(noteInfo));
 
-});  
+});
 
 // //api post router
 // router.post("/notes", (req, res) => {
@@ -42,20 +51,23 @@ router.post("/notes", (req, res) => {
 //     res.json(noteInfo)
 //     console.log(noteID);
 // }); 
-  
+
 // //api get router
 // router.get("/notes", (req, res) => {
 //   let noteInfo = JSON.parse(fs.readFileSync("./db/db.json"));
 //     res.json(noteInfo)
-    
-  
+
+
 // });
 
 //delete
 router.delete('/notes/:id', (req, res) => {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); // 
-    let  = savedNotes.filter(x=>x.id!=req.params.id) 
-fs.writeFileSync("./db/db.json", JSON.stringify(noteID));
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID= savedNotes.filter(x=>x.id!=req.params.id) 
+fs.writeFileSync("./db/db.json", JSON.stringify(noteID), (err) => {
+    if (err) throw err;
+});
+return res.json(savedNotes);
 });
 
 
@@ -65,3 +77,5 @@ fs.writeFileSync("./db/db.json", JSON.stringify(noteID));
 
 
 module.exports = router;
+
+
